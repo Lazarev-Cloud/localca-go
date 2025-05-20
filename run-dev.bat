e66454799374544b71a9050bf9334caa6f65956a
lazarevtill <lazarevtill@gmail.com>
2025-05-21 00:59:17 +0200

    Update run-dev.bat

--- run-dev.bat
+++ run-dev.bat
@@ -5,7 +5,7 @@ set ORGANIZATION=LocalCA Organization
 set COUNTRY=US
 set DATA_DIR=./data
 set LISTEN_ADDR=:8080
-set CA_KEY=development-password
+set CA_KEY_FILE=cakey.txt
 
 REM Run the application
 go run main.go 
